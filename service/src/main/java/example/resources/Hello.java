e9e2fc53c6039e7a288d2abfeed65e5750f2c9a8
Nicolas Fagotti <nfagotti@cloud.upwork.com>
2023-02-28 17:00:46 -0300

    Add status endpoint

--- service/src/main/java/example/resources/Hello.java
+++ service/src/main/java/example/resources/Hello.java
@@ -3,6 +3,7 @@ package example.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("hello")
@@ -12,5 +13,12 @@ public class Hello {
     @Path("{text}")
     public Response displayHelloText(@PathParam("text") String text) {
         return Response.ok(String.format("Hello %s! Good afternoon!!!", text)).build();
+    }
+
+    @Path("/status")
+    @GET
+    public Response getStatus(@QueryParam("statusResponse") Integer statusResponse) {
+        statusResponse = statusResponse != null ? statusResponse : 200;
+        return Response.status(statusResponse, String.format("Status returned: %d", statusResponse)).build();
     }
 }
